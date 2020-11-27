dc82cbe3bb4eedf5718c765283c0373d05cef44e
sif aldin abbas <saif.m.abbas@gmail.com>
2020-11-27 15:19:38 +0100

    create image uploader

--- frontend/src/components/profile/ProfilePage.jsx
+++ frontend/src/components/profile/ProfilePage.jsx
@@ -4,7 +4,25 @@ import "../../css/Components/profilePage/profile.css";
 
 export default function ProfilePage() {
 
+    const uploadedImage = React.useRef(null);
+    const imageUploader = React.useRef(null);
+
+    const handleImageUpload = e => {
+        const [file] = e.target.files;
+        if (file) {
+            const reader = new FileReader();
+            const { current } = uploadedImage;
+            current.file = file;
+            reader.onload = e => {
+                current.src = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
+
     const [user, setUser] = useState('');
+
 
     useEffect(() => {
         Api.get("/user/me").then((response) => {
@@ -17,11 +35,53 @@ export default function ProfilePage() {
 
 
     return (
-        
-        <div className="profilePage">
-            <h1>{user.name}</h1>
 
+        <div className="profilePage">
+
+
+            <div 
+                style={{
+                    height: "10rem",
+                    width: "10rem",
+                    border: "1px solid black",
+                    borderRadius: "50%"
+                }}
+                onClick={() => imageUploader.current.click()}
+            >
+                <img
+                    ref={uploadedImage}
+                    style={{
+                        width: "100%",
+                        height: "100%",
+                        position: "relative",
+                        borderRadius: "50%"
+
+                    }}
+                />
+                  <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageUpload}
+                ref={imageUploader}
+                style={{
+                    display: "none"
+                }}
+            />
+
+            </div>
+
+           <div>
+           <h1>{user.name}</h1>
             <h2>{user.email}</h2>
+           </div>
+           
+            
+
+
+
+
         </div>
+
+
     )
 }
