c0cc8d9a9b1e617b23374db0997d768e71b24dbc
sif aldin abbas <saif.m.abbas@gmail.com>
2020-11-27 10:35:41 +0100

    start profiling page

---
+++ frontend/src/components/profile/ProfilePage.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import Api from "../../api/Api";
+
+export default function ProfilePage() {
+    return (
+        <div>
+
+        </div>
+    )
+}
