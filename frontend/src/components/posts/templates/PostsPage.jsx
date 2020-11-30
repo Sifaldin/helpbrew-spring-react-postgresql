46ba0096fd17dd7d51ab8db684f968505c64126c
awesome-button <oleshko.lena@gmail.com>
2020-11-30 17:11:32 +0100

    Refactored PostsPage so the posts fetching happens in it parent component App

--- frontend/src/components/posts/templates/PostsPage.jsx
+++ frontend/src/components/posts/templates/PostsPage.jsx
@@ -4,19 +4,10 @@ import Api from "../../../api/Api";
 
 //The PostsPage displays a listing of posts belonging to the category(skills, giveaways or monetary support)
 // selected by the user
-export default function PostsPage({ category }) {
-  const [posts, setPosts] = useState([]);
-
-  //The component fetches posts belonging to the category passed as props.
-  //The category string is inserted into the endpoint ensuring that
-  //the posts from the right category are fetched.
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await Api.get(`/posts/category/${category}`);
-      setPosts(response.data);
-    };
-    fetchPosts();
-  }, [category]); //refetches posts every time category changes
+export default function PostsPage({ category, posts }) {
+  const getPosts = () => {
+    return posts.filter((post) => post.category === category);
+  };
 
   return (
     <div className="posts">
@@ -24,7 +15,7 @@ export default function PostsPage({ category }) {
       {/* The fetched posts are mapped through and a post card is displayed
       for each of the posts by PostCard component.
       */}
-      {posts.map((post) => (
+      {getPosts().map((post) => (
         <PostCard key={post.id} post={post} />
       ))}
     </div>
