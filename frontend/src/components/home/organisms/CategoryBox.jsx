526f27ddb5353eb077016b799c2c52350ff63dbc
awesome-button <oleshko.lena@gmail.com>
2020-12-02 15:31:27 +0100

    added toggling between user's requested and givenposts to the dashboard

--- frontend/src/components/home/organisms/CategoryBox.jsx
+++ frontend/src/components/home/organisms/CategoryBox.jsx
@@ -2,10 +2,22 @@ import giveaways from "../../../assets/giveaways.jpg";
 import skills from "../../../assets/skills.jpg";
 import monetarySupport from "../../../assets/monetary-support.jpg";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 //CategoryBox is a box inside the dashboard. It displays posts within a certain category posted by
 //the logged in user.
 export default function CategoryBox({ category, posts }) {
+  const [type, setType] = useState("requested");
+
+  //the variable stores only user's give posts
+  const givePosts = posts.filter((post) => post.postType === "give");
+  //the variable stores only user's request posts
+  const requestPosts = posts.filter((post) => post.postType === "request");
+
+  //this state is used to have the right posts(requested or given) to map through in this component
+  const [postsByType, setPostsByType] = useState(requestPosts);
+
   //removes dashes and capitilizes the category. ex. from "monetary-support" to "Monetary support"
   const formatCategory = (category) => {
     const withSpace = category.split("-").join(" ");
@@ -37,21 +49,34 @@ export default function CategoryBox({ category, posts }) {
         {/* toggle thing to add */}
         <h2>{formatCategory(category)}</h2>
 
-        {/* !!! Toggle to be implemented once Hassan includes it into NewPostForm */}
-        <span>toggle</span>
+        {/* Toggling the button(requested/given) allows to see only posts requested or given by user */}
+        <button
+          onClick={() => {
+            if (type === "requested") {
+              setType("given");
+              setPostsByType(givePosts);
+            } else {
+              //when type equals "given"
+              setType("requested");
+              setPostsByType(requestPosts);
+            }
+          }}
+        >
+          {type} <RiArrowDropDownLine />
+        </button>
       </div>
 
       {/* the bottom of the box contains category name with a number of posts and posts icons and titles */}
       <div className="bottom">
         <span className="category-name">
-          Your {formatCategory(category)}({posts.length})
+          Your {formatCategory(category)}({postsByType.length})
         </span>
 
         {/* !!! The posts below will be displayed in a different way depending on the category
         This stays to be done during next iteration!
         */}
         <ul className="post-icons">
-          {posts.map((post) => {
+          {postsByType.map((post) => {
             return (
               <div className="post-icon">
                 <Link to={{ pathname: `/posts/${post.id}`, state: { post } }}>
