2c7d07597c2d0fffab1f7928882149577da6df4b
Yididiya M. Gudisa <yididiyamulatu@gmail.com>
2020-12-08 10:11:45 +0100

    visual edits for messgaes

--- frontend/src/components/chat/ThreadPage.jsx
+++ frontend/src/components/chat/ThreadPage.jsx
@@ -33,13 +33,9 @@ function ThreadPage() {
           <div className="inbox_people">
             <div className="headind_srch">
               <div className="recent_heading">
-                <h4>Recent</h4>
+                <h4>Messages</h4>
               </div>
-              <div className="srch_bar">
-                <div className="stylish-input-group">
-                  <input type="text" className="search-bar" placeholder="Search" />
-                </div>
-              </div>
+              
             </div>
             <div className="inbox_chat scroll">
               <div className="chat_list active_chat">
