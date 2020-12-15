b0d2bda7d2bc6104db85e00c0dc1e3e1ff962656
sif aldin abbas <saif.m.abbas@gmail.com>
2020-12-15 12:25:22 +0100

    new login page

--- frontend/src/components/auth/LoginPage.jsx
+++ frontend/src/components/auth/LoginPage.jsx
@@ -1,4 +1,4 @@
-import {React, useState} from 'react';
+import { React, useState } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Auth from '../../services/Auth';
@@ -23,45 +23,44 @@ function LoginPage() {
 
   const [isMember, setIsMember] = useState(true);
 
-  const goRegister =() =>{
+  const goRegister = () => {
     setIsMember(false);
   }
-  const goLogin =() =>{
+  const goLogin = () => {
     setIsMember(true);
   }
 
   return (
     <div className="loginpage">
-      <div>
-        <div>
-          <Router>
-            <div className="logo">
-              <img src={logo} alt= "HelpBrew Logo" />
-              <div className="loginpage-text">
-              <h5>With HelpBrew, you can share and receive help with skills, give away stuff, receive and give monetary support. HelpBrew gives us all the opportunity to make a difference and create a better world. </h5>
-              </div>
-              
-            </div>
-        </Router>
-              {/*<<input id="tab-1" type="radio" name="tab"  checked />
+
+      <div className="loginGrid">
+        <div className="loginpage-text">
+          
+            <img src={logo} alt="HelpBrew Logo" />
+            <h3>With HelpBrew, you can share and receive help with skills, give away stuff,
+            receive and give monetary support. HelpBrew gives us all the opportunity to make a
+              difference and create a better world. </h3>
+       
+        </div>
+        {isMember ?
+          <LoginForm onSubmit={login} goRegister={goRegister} />
+          : <RegisterForm onSubmit={register} goLogin={goLogin} />}
+
+
+        {/*<<input id="tab-1" type="radio" name="tab"  checked />
               <label htmlFor="tab-1" >
                 Login
               </label>
               <input id="tab-2" type="radio" name="tab"  />
               <label htmlFor="tab-2" >
                 Sign Up
-              </label>
-  <div >*/}
-                {isMember ? 
-              <LoginForm onSubmit={login} goRegister = {goRegister}/> 
-              : <RegisterForm onSubmit={register} goLogin = {goLogin}/>}
-              </div>
-              
-            </div>
-           
+              </label> <div >*/}
 
-          </div>
-          
+
+      </div>
+
+    </div>
+
   );
 }
 
